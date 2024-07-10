41623246d69c0abd0d996cb1258a9b146caa55ee
Oliver Silvester <oliver.silvester@diamond.ac.uk>
2024-07-10 09:04:31 +0000

    calculator tests finished

--- src/htss/calculator-2.py
+++ src/htss/calculator-2.py
@@ -1,4 +1,5 @@
-
+import pytest
+from unittest.mock import patch
 class ValueNegative(Exception):
     pass
 class ValueOdd(Exception):
@@ -8,8 +9,8 @@ class TotalOdd(Exception):
 
 def calculator():
     try:
-        num1 = input(">")
-        num2 = input(">")
+        num1 = int(input(">"))
+        num2 = int(input(">"))
         if num1 < 0 or num2 < 0:
             raise ValueNegative
         elif num1 % 2 != 0 or num2 % 2 != 0:
@@ -20,16 +21,43 @@ def calculator():
         else:
             return num1, num2
     except ValueNegative:
-        print("Idk")
+        print("Inputs must be positive.")
     except ValueOdd:
-        print("Haha")
+        print("Inputs must be even.")
     except TotalOdd:
-        print("Something")
-    except TypeError:
-        print("Type error but i defined it.")
+        print("Total must be an even number.")
+    except ValueError:
+        print("Inputs must be integers or floats.")
+
+@patch("builtins.print")
+def test_if_inputs_are_negative_custom_exception_raised(mock_print):
+    with patch("builtins.input", side_effect = [-2,4]):
+        calculator()
+    mock_print.assert_called_with("Inputs must be positive.")
+
+@patch("builtins.print")
+def test_if_inputs_are_odd_custom_exception_raised(mock_print):
+    with patch("builtins.input", side_effect = [3,2]):
+        calculator()
+    mock_print.assert_called_with("Inputs must be even.")
 
 
-calculator()
+def test_if_inputs_and_total_valid_return_user_inputs():
+    with patch("builtins.input", side_effect = [6,8]):
+        calculator()
+     
+
+@patch("builtins.print")
+def test_if_type_error_handled_appropriately(mock_print):
+    with patch("builtins.input", side_effect = ["arrow", True]):
+        calculator()
+    mock_print.assert_called_with("Inputs must be integers or floats.")
 
 
+    
 
+
+# test_if_inputs_are_negative_custom_exception_raised()
+# test_if_inputs_are_odd_custom_exception_raised()
+# test_if_inputs_and_total_valid_return_user_inputs()
+test_if_type_error_handled_appropriately()
