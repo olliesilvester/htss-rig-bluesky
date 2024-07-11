9a63a1403b88deef7767bbb4a28e49cb69ab44d5
Oliver Silvester <oliver.silvester@diamond.ac.uk>
2024-07-11 11:21:09 +0000

    new

--- src/htss/valentina/thursday_intro.py
+++ src/htss/valentina/thursday_intro.py
@@ -3,26 +3,29 @@ import time
 from htss import run_plans_here
 from htss.devices import BacklightPower
 from htss.run_plans_here import be
-from htss.run_plans_here import det
-from bluesky.plans import count
+from htss.run_plans_here import detector, sam
+from bluesky.plans import count, scan
 import bluesky.plan_stubs as bps
 from htss.run_plans_here import RE
+from ophyd_async.epics.motion import Motor
+
+dets = [detector]
 
 #intro 
 
 def turn_light_on_and_off():
-    yield from bps.abs_set(be.power, BacklightPower.ON, wait=True)
+    yield from bps.abs_set(be.power, BacklightPower.OFF, wait=True)
     time.sleep(5)
-    be.power = BacklightPower.ON
+    yield from bps.abs_set(be.power, BacklightPower.ON, wait=True)
 
 def count_detector():
-    yield from count(det, num=1)
+    yield from count(dets, num=1)
 
-def move_motor_x():
-    yield from bps.mv(scan(det, move_motor_x, 1))
+def move_motor_x(value):
+    yield from bps.mv(sam.x, value)
 
-def move_motor_a():
-    yield from bps.mv(scan(det, move_motor_a, 1))
+def move_motor_a(value):
+    yield from bps.mv(sam.theta, value)
 
 #part one
 
@@ -30,11 +33,32 @@ def light_on():
     yield from bps.abs_set(be.power, BacklightPower.ON, wait=True)
 
 def move_a_zero():
-    yield from bps.mv(scan(det, move_motor_a, 0))
+    yield from bps.mv(scan(dets, sam.theta, 0))
 
 def takes_five_images():
-    for i in range(5):
-        yield from scan(det, num = 1)
+    yield from scan(dets, sam.theta, 0, 80, num = 5)
 
 def light_off():
     yield from bps.abs_set(be.power, BacklightPower.OFF, wait=True)
+
+
+def combined():
+    yield from bps.abs_set(be.power, BacklightPower.ON, wait=True)
+    yield from bps.mv(sam.theta, 0)
+    yield from scan(dets, sam.theta, 0, 80, num = 5)
+    yield from bps.abs_set(be.power, BacklightPower.OFF, wait=True)
+
+
+#RE(combined())
+
+#part two
+class parameter_outside_limits(Exception):
+    pass
+
+
+async def set(self, x_pos: float, y_pos: float):
+    val = await Motor.low_limit_travel.get_value()
+    val2 = await Motor.high_limit_travel.get_value()
+    if val < Motor.low_limit_travel or val2 > Motor.high_limit_travel:
+        raise parameter_outside_limits
+    
